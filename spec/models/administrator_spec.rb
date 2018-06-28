8085b4be48c1b4b07e0230211fcb43780ef0a681
Carl <kassa096@live.com>
2018-06-28 07:15:47 +0000

    Issue #10 - adding attribute spec tests

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Administrator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it "has a name attribute" do
+      expect(FactoryBot.build(:administrator, name: "name", email_address: "address@email.com")).to respond_to(:name)
+    end
+    
+    it "has an email address attribute" do
+      expect(FactoryBot.build(:administrator, name: "name", email_address: "address@email.com")).to respond_to(:email_address)
+    end
+    
+    it "has a password digest attribute" do 
+      expect(FactoryBot.build(:administrator, name: "name", email_address: "address@email.com")).to respond_to(:password_digest)        
+    end
+  end
 end
