34f6788d92afce42c532b5ca56bcb5e79e5690b9
Carl <kassa096@live.com>
2018-06-28 07:10:21 +0000

    Issue #10 - adding administrator model

--- features/support/fixtures/administrator.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-    factory :administrator do
-        name 'name'
-        email 'email@email.com'
-        password 'password'
-        password_confirmation 'password'
-    end
-end
