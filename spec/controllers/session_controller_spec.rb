72436d27ef3577bd3edbb8cc6b46e206fcfd53d9
Carl <kassa096@live.com>
2018-06-28 07:48:56 +0000

    Issue #10 - adding login route

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SessionController, type: :controller do
-
+    it 'routes get /login to session#new' do
+        expect(get: "/administration/login").to route_to( controller: "session", action: "new")        
+    end
 end
