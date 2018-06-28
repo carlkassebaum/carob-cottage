82401e367b13b20a39048b7ed1b6f1744656424f
Carl <kassa096@live.com>
2018-06-28 06:14:01 +0000

    Issue #10 - adding step definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,28 @@
+require 'cucumber/rails'
+require 'uri'
+require 'cgi'
+require 'factory_girl'
+
+module NavigationHelpers
+  def path_to(page_name)
+    case page_name
+
+    when /^the home\s?page$/
+      '/'
+    else
+      begin
+        page_name =~ /^the (.*) page$/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue NoMethodError, ArgumentError
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n"
+      end
+    end
+  end
+end
+World(NavigationHelpers)
+
+
+Given("administrators with the following attributes exists:") do |table|
+  FactoryGirl.create(:administrator, name: "bob")
+end
