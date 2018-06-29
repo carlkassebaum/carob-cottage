45efc851222ec018a32d9150e6b08b67846e1281
Carl <kassa096@live.com>
2018-06-29 12:21:15 +0000

    Issue #14 - adding cost attribute to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_121738) do
+ActiveRecord::Schema.define(version: 2018_06_29_122027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_06_29_121738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "contact_number"
+    t.integer "cost"
   end
 
 end
