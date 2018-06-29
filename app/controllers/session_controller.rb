b8a7e4f4c87ce68b6f0dddd5acbdc56af98c205f
Carl <kassa096@live.com>
2018-06-29 01:04:21 +0000

    Issue #10 - adding login redirection

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,6 +7,9 @@ class SessionController < ApplicationController
         if admin && admin.authenticate(params[:session][:password])
             session[:user_id] = admin.id
             redirect_to administration_path
+        else
+            flash[:alert] = "Invalid email address or password given"
+            redirect_to administration_login_path
         end
     end
 end
