b12a168dfcb5b8d99b854f2dfe17b22f8f102e2c
Carl <kassa096@live.com>
2018-07-01 12:03:44 +0000

    Issue #14 - adding methods to retrieve bookings within a date range

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,8 @@
 class Booking < ApplicationRecord
+    #Get the bookings with date attributes between the date range for the arrival and departure dates
+    def self.bookings_within(date_range)
+        arrivals   = Booking.where(:arrival_date => date_range)
+        departures = Booking.where(:departure_date => date_range)
+        (arrivals + departures).uniq
+    end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
+  describe "bookings_within" do
+    before :each do 
+      @booking_1 = FactoryBot.create(:booking, name: "test_1", arrival_date: "20-1-2018", departure_date: "25-1-2018")
+      @booking_2 = FactoryBot.create(:booking, name: "test_2", arrival_date: "31-1-2017", departure_date: "5-1-2018")
+      @booking_3 = FactoryBot.create(:booking, name: "test_3", arrival_date: "15-5-2017", departure_date: "25-5-2017")
+      @booking_4 = FactoryBot.create(:booking, name: "test_4", arrival_date: "29-5-2018", departure_date: "3-6-2018")
+      @booking_5 = FactoryBot.create(:booking, name: "test_5", arrival_date: "1-8-2019",  departure_date: "2-10-2019")
+      @booking_6 = FactoryBot.create(:booking, name: "test_6", arrival_date: "5-3-2018",  departure_date: "10-3-2018")
+    end
+    
+    it "returns bookings with-in the given date range but ignores those beyond the date range" do
+      date_range  = Date.new(2018,1,1)..Date.new(2018,12,31)
+      test_result = Booking.bookings_within(date_range)
+      expect(test_result).to eq([@booking_1, @booking_4, @booking_6, @booking_2])
+    end
+  end
+  
   describe "attributes" do
     it "has a name attribute" do
       expect(FactoryBot.build(:booking)).to respond_to(:name)
