7dac31a083516ad1d161b7451a457c0b3b78013a
Carl <kassa096@live.com>
2018-07-04 03:08:41 +0000

    Issue #22 - styling changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-#Booking.create(name: "test_1", arrival_date: "20-1-2018", departure_date: "26-1-2018", status: "reserved")
-#Booking.create(name: "test_2", arrival_date: "26-1-2018", departure_date: "2-2-2018",  status: "reserved")
+Booking.create(name: "test_1", arrival_date: "20-1-2018", departure_date: "26-1-2018", status: "reserved")
+Booking.create(name: "test_2", arrival_date: "26-1-2018", departure_date: "2-2-2018",  status: "reserved")
 Booking.create(name: "test_3", arrival_date: "2-2-2018",  departure_date: "8-2-2018",  status: "booked")
