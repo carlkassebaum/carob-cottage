5b8e13ea3a8c14a50eb4400292706c592f7d548a
Carl <kassa096@live.com>
2018-07-19 06:54:53 +0000

    issue #42 - nav bar sizing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -204,6 +204,19 @@ $(document).one('click','body *',function()
 var slideIndex = 1;
 var timer;
 
+function fill()
+{
+  var nav_bar    = document.getElementById("customer_navigation_bar");
+  var nav_fillers = document.getElementsByClassName("nav_bar_filler");
+  var side_bar_fillers = document.getElementsByClassName("side_bar_filler");
+  if(nav_fillers != null && nav_fillers.length > 0)
+    for(var i = 0; i < nav_fillers.length; i++)
+      nav_fillers[i].style.height = nav_bar.offsetHeight.toString() + "px";    
+  if (side_bar_fillers != null && side_bar_fillers.length > 0)
+    for(var i = 0; i < side_bar_fillers.length; i++)
+      side_bar_fillers[i].style.height = (nav_bar.offsetHeight.toString() - 45) + "px";    
+}
+
 $(document).ready(function() 
 {  
   if (window.location.pathname == "/")
@@ -211,4 +224,9 @@ $(document).ready(function()
     var gallery = document.getElementById("home_gallery_body");
     gallery.style.display = "none";
   }
+});
+
+$(window).resize(function () 
+{ 
+  fill(); 
 });
