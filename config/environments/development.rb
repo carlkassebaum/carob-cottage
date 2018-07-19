8c6f6ffc0eb5c586835bb71ae6cd215140a7f1ee
Carl <kassa096@live.com>
2018-07-19 23:20:06 +0000

    using env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'staycarobcottage@gmail.com',
-    password:             'carobtree101!',
+    password:             '<password>',
     authentication:       'plain',
     enable_starttls_auto: true 
   }  
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'staycarobcottage@gmail.com',
-    password:             'carobtree101!',
+    user_name:            ENV['MAILER_EMAIL'],
+    password:             ENV['MAILER_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true 
   }
