d101d32af2278a589329dd2604b62a336d31b46d
Carl <kassa096@live.com>
2018-07-19 06:57:06 +0000

    issue #42 - removing unused specs

--- spec/routing/static_pages_routing_spec.rb
+++ spec/routing/static_pages_routing_spec.rb
@@ -4,10 +4,4 @@ RSpec.describe StaticPagesController, type: :routing do
             expect(get: "/").to route_to( controller: "static_pages", action: "home")
         end
     end
-    
-    describe "get /about_the_area" do
-        it "routes get /about_the_area to static_pages#home" do
-            expect(get: "/about_the_area").to route_to( controller: "static_pages", action: "location")
-        end        
-    end
 end
