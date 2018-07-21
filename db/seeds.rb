273b74453ffbedfbd295178cc2ff87ba0d33dc9f
Carl <kassa096@live.com>
2018-07-21 01:16:31 +0000

    correcting image allignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 case Rails.env
+
 when "development"
     FactoryBot.create(  :administrator,
                         name: "bob",
