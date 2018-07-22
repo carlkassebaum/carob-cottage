253bf79243b41253562d2586fe415c8cc1bf1175
Carl <kassa096@live.com>
2018-07-22 03:24:57 +0000

    Fixing spelling errors

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,6 @@ Rails.application.routes.draw do
   
   get 'about'                => "static_pages#about"
   
-  get "tarriff"              => "static_pages#tarrif"
+  get "tariff"               => "static_pages#tarrif"
 
 end
